be6e55dbf947bb54e9e9f6ea8d901b16663b999a
eleanor <44651387+elykittytee@users.noreply.github.com>
2022-08-26 11:36:31 -0500

    created main.py
    student version of main.py for 1.04

---
+++ Unit 1/04_lesson/main.py
@@ -0,0 +1,13 @@
+# Lesson 1.04: Variables and User Input
+# You may include print statements with line breaks for easier readability
+
+### Do Now
+# Complete the Do Now activity here
+
+#### For Your Notes
+# Type any notes you have here
+
+### Practice Input Statements
+# [A] Write your program below this line
+
+# [B] Write your program below this line
